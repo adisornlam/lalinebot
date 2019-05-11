2ca7db629154a41f72e550faab71ad565f337105
Adisorn Lamsombuth <postyim@gmail.com>
2019-05-11 22:41:39 +0700

    line

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/webhook', 'API/UserController@index');
+Route::get('/webhook', 'API/LineController@index');
